68785ea3b3f873f8a6b33e025b2b9e377c895142
juanlumoreno <59013470+juanlumoreno@users.noreply.github.com>
2021-03-07 00:53:33 +0100

    Update Login.tsx

--- client-myracketpartner/src/components/Generic/Modals/Login.tsx
+++ client-myracketpartner/src/components/Generic/Modals/Login.tsx
@@ -1,6 +1,24 @@
 // DEPENDENCES
 import React from "react";
 import Modal from 'react-bootstrap/Modal';
+import styled from 'styled-components';
+
+// STYLED-COMPONENTS
+const Input = styled.input`
+    width: 100%;
+    margin: 5px 0;
+`;
+
+const SignInBtn = styled.button.attrs({
+    children: 'Sign In'
+})`
+    width: 100%;
+    margin: 5px 0;
+`;
+
+const FooterWrapper = styled.div`
+    background-color: #ECF0F1;
+`;
 
 // TYPESCRIPT
 interface IProps {
@@ -17,20 +35,28 @@ const LoginModal: React.FC<IProps> = props => {
             show={show}
             onHide={handleClose}
             keyboard={false}
+            size="sm"
             aria-labelledby="contained-modal-title-vcenter"
             centered
         >
         <Modal.Header closeButton>
             <Modal.Title>Login</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Login Form</Modal.Body>
+        <Modal.Body>
+            <Input type="text" placeholder="Email" />
+            <Input type="password" placeholder="Password" />
+            <SignInBtn />
+        </Modal.Body>
         <Modal.Footer>
-            <button onClick={handleClose}>
+            <FooterWrapper>
+            {/* <button onClick={handleClose}>
                 Close
             </button>
             <button onClick={handleClose}>
                 Sign In
-            </button>
+            </button> */}
+            <a href="/#"> Forgot Password? </a>
+            </FooterWrapper>
         </Modal.Footer>
         </Modal>
     );
